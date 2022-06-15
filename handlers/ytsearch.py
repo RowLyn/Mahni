3625f790539cf4e0c24728df864023fcda7ed27b
RowLyn <95884044+RowLyn@users.noreply.github.com>
2022-06-15 19:54:16 +0400

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -16,7 +16,7 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["axtar"]))
+@app.on_message(pyrogram.filters.command(["ytaxtar"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
